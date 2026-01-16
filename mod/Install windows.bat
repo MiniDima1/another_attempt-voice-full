303d179548913b7c5dc264fab912d67e9aff46da
MiniDima1 <88030317+MiniDima1@users.noreply.github.com>
2026-01-16 15:12:16 +0700

    Delete mod directory

--- mod/Install windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-xcopy "another_attempt" "C:\Program Files (x86)\Steam\steamapps\common\Altushka dlya skufa\game\another_attempt" /I /E
-echo Готово!
-echo Мод установлен
-pause
